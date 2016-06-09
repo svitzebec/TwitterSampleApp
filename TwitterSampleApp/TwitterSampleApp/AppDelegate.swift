66d5f0f80bb77ac91e91360189ecbdb3b7d07df9
Svit Zebec <svit.zebec@gmail.com>
2016-06-09 09:56:59 +0200

    Fabric TwitterKit initialization

--- TwitterSampleApp/TwitterSampleApp/AppDelegate.swift
+++ TwitterSampleApp/TwitterSampleApp/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
+		Fabric.with([Twitter.self])
 		return true
 	}
 
